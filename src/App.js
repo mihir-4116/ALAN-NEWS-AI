3c71d4a3557b3cd7195b1b44fbec1c5fb90f7feb
mihir-4116 <77438541+mihir-4116@users.noreply.github.com>
2021-07-20 17:50:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
 
   useEffect(() => {
     alanBtn({
-      key: 'a0b09762904881bc34dcc746eb0769b02e956eca572e1d8b807a3e2338fdd0dc/stage',
+      key: 'add your own api key here',
       onCommand: ({ command, articles, number }) => {
         if (command === 'newHeadlines') {
           setNewsArticles(articles);
